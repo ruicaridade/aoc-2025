51001f9f5d0f90ab5c1abd1501409e046ea0d520
Rui Caridade <ruidanielcaridade@gmail.com>
2025-12-01 19:05:51 +0000

    fix day 1 with brute force method

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,39 +1,42 @@
-const MAX: i32 = 100;
+const MAX: i32 = 99;
 
 fn main() {
     let lines = common::read_lines_from_input();
 
     let mut part_one: i32 = 0;
     let mut part_two: i32 = 0;
-
-    let mut current_number: i32 = 50;
+    let mut position: i32 = 50;
 
     for line in lines {
         let (direction, steps_str) = line.split_at(1);
 
         let steps = steps_str.parse::<i32>().unwrap();
-        let rotations = steps / MAX;
-        let remainder = steps % MAX;
-
         let multiplier: i32 = match direction {
             "L" => -1,
             "R" => 1,
             _ => panic!("Invalid direction: {}", direction),
         };
 
-        let mut new_number = current_number + remainder * multiplier;
+        let mut p = position;
+        for _ in 0..steps {
+            p += multiplier;
 
-        part_two += rotations;
-        if (multiplier == 1 && new_number >= MAX) || (multiplier == -1 && new_number < 1) {
-            part_two += 1;
+            if p > MAX {
+                p = 0;
+            } else if p < 0 {
+                p = MAX;
+            }
+
+            if p == 0 {
+                part_two += 1;
+            }
         }
 
-        new_number = new_number % MAX;
-        if new_number == 0 {
+        if p == 0 {
             part_one += 1;
         }
 
-        current_number = new_number;
+        position = p;
     }
 
     println!("Part 1: {}", part_one);
